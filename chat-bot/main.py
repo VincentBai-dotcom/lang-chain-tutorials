e265b1a6ba2b76c18a1866915f0000969bf7cf12
VincentBai-dotcom <butcheryyy@gmail.com>
2025-08-30 22:42:59 -0700

    adding model invocation code

--- chat-bot/main.py
+++ chat-bot/main.py
@@ -1,5 +1,25 @@
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
 def main():
+    """Main function to run the chat bot with Google Gemini model."""
     print("Hello from chat-bot!")
+    load_dotenv()
+    if not os.environ.get("GOOGLE_API_KEY"):
+        os.environ["GOOGLE_API_KEY"] = getpass.getpass(
+            "Enter API key for Google Gemini: "
+        )
+
+    model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+    messages = [
+        SystemMessage(content="Translate the following from English into Italian"),
+        HumanMessage(content="hi!"),
+    ]
+    print(model.invoke(messages))
 
 
 if __name__ == "__main__":
