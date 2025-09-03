03cb7a4ceeab51c7cf92ff64b2c6f04d9dbece92
VincentBai-dotcom <butcheryyy@gmail.com>
2025-09-02 23:08:23 -0700

    complete classification

--- classification/main.py
+++ classification/main.py
@@ -6,7 +6,16 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel, Field
 
-
+class Classification(BaseModel):
+    sentiment: str = Field(..., enum=["happy", "neutral", "sad"])
+    aggressiveness: int = Field(
+        ...,
+        description="describes how aggressive the statement is, the higher the number the more aggressive",
+        enum=[1, 2, 3, 4, 5],
+    )
+    language: str = Field(
+        ..., enum=["spanish", "english", "french", "german", "italian"]
+    )
 
 def load_env():
     load_dotenv()
@@ -16,8 +25,24 @@ def load_env():
 def main():
     print("Hello from classification!")
     load_env()
-    model = init_chat_model(model="gemini-2.5-flash", model_provider="google_genai")
-    print(model.invoke("Hello, world!"))
+    llm = ChatOpenAI(temperature=0, model="gpt-4o-mini").with_structured_output(
+        Classification
+    )
+    tagging_prompt = ChatPromptTemplate.from_template(
+    """
+    Extract the desired information from the following passage.
+
+    Only extract the properties mentioned in the 'Classification' function.
+
+    Passage:
+    {input}
+    """
+    )
+    
+    inp = "Estoy muy enojado con vos! Te voy a dar tu merecido!"
+    prompt = tagging_prompt.invoke({"input": inp})
+    response = llm.invoke(prompt)
+    print(response.model_dump())
 
 
 if __name__ == "__main__":
